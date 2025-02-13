e03c61438e1f0d37b1a3da33e1f7f559ca00064e
shakeel <shakeel.a@madstreetden.com>
2025-02-13 18:09:22 +0530

    connected slackbot

--- deepseek_LLM_server.py
+++ deepseek_LLM_server.py
@@ -1,38 +1,22 @@
-import requests
-import json
 import re
+import ollama
 
-llm_host = 'http://localhost:11434/api/generate'
 
 LLM_MODEL = "deepseek-r1:listenToAktrxx"
 
-def llm_chatbot(prompt):
+def llm_deepseek(prompt):
 
-    payload = {
-    'model': LLM_MODEL,
-    "prompt": prompt,
-    }
+    # print("prompt")
+    # print()
+    # print(prompt)
+    # print()
 
-    response = requests.post(llm_host, json=payload, stream=True)
-
-    chat_response = ''
-    if response.status_code==200:
-        print("Generated text", end=' ', flush=True)
-        for line in response.iter_lines():
-            if line:
-                decode_line = line.decode('utf-8')
-                result = json.loads(decode_line)
-                generated_text = result.get("response", "")
-                chat_response += generated_text
-                print("\rThinking...", end='', flush=True)
-        print(chat_response)
-
-        cleaned_text = re.sub(r'<think>.*?</think>\s*', '', chat_response, flags=re.DOTALL)
-
-        return cleaned_text
-    else:
-        print('Error')
-        return "Error"
+    chat_res = ollama.chat(
+        model=LLM_MODEL,
+        messages= prompt
+        )
+    cleaned_text = re.sub(r'<think>.*?</think>\s*', '', chat_res['message']['content'], flags=re.DOTALL)
+    return cleaned_text
 
 if __name__ == "__main__":
-    llm_chatbot('Hi hello')
+    llm_deepseek("Hi, what is your name?")
--- slackbot.py
+++ slackbot.py
@@ -3,6 +3,7 @@ import os
 import json
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+from slack_sdk import WebClient
 from collections import deque
 from deepseek_LLM_server import *
 
@@ -10,35 +11,58 @@ from deepseek_LLM_server import *
 SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
 
 SLACK_APP_TOKEN = os.getenv("SLACK_APP_TOKEN")
+SYSTEM_BASE_BEHAVIOUR = "You are the chatbot agent named 'Ana' in a company named 'Doctor Droid', your job is to give answers to the questions about the company in the slack channel, every inputs comes to you is from Slack msg, respond to them accordingly, Note down the fllowing information about the company, your answers shoudl be based on the following. Company Name: Doctor Droid, Company Location: Bengaluru, India. Company founders are Siddarth Jain and Dipesh Mittal. you called by your username, help the user who asking questions, Answer withing two or three sentence, dont give long answers more than 600 characters"
 
 app = App(token=SLACK_BOT_TOKEN)
+client = WebClient(token=SLACK_BOT_TOKEN)
 
 message_history = {}
 
+
+def get_user_name(user_id):
+    user_info = client.users_info(user=user_id)
+    return user_info["user"]["real_name"]
+
+
+
+def process_query(thread_ts, sender_name, user_query  ):
+    if thread_ts not in message_history:
+        message_history[thread_ts] = deque(maxlen=5)
+        # message_history[thread_ts] .append({"role": "system", "content": "You are a helpful assistant"})
+    
+    message_history[thread_ts].append({"role": "user", "content": user_query})
+
+    context_messages = list(message_history[thread_ts])
+
+    # print(context_messages)
+    context_messages.insert(0, {"role": "system", "content": SYSTEM_BASE_BEHAVIOUR})
+    reply_text = llm_deepseek(context_messages)
+    message_history[thread_ts].append({"role": "assistant", "content": reply_text})
+    
+    # print("Message History:")
+    # print()
+    # print(message_history)
+    # print()
+
+    
+    return reply_text
+
+
+
 @app.event("app_mention")
 def handle_mention(event, say):
-    print("Event log")
     channel_id = event["channel"]
     user_query = event["text"]
     thread_ts = event.get("thread_ts", event["ts"]) 
 
-    if channel_id not in message_history:
-        message_history[channel_id] = deque(maxlen=5)
-    
+    sender_name = get_user_name(event["user"])
 
-    message_history[channel_id].append(user_query)
+    reply_text = process_query(thread_ts, sender_name, user_query)
 
-    context_messages = list(message_history[channel_id])
-    
-    
-    reply_text = llm_chatbot(user_query)
-    
-
-    say(text=reply_text)
-    # say(text=reply_text)
+    say(text=reply_text, thread_ts=thread_ts)
 
 
-# Start the app
+
 if __name__ == "__main__":
     handler = SocketModeHandler(app, SLACK_APP_TOKEN)
     handler.start()
